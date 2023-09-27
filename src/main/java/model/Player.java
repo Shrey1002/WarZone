cd4ef604e52ea7859a86f2be07b74b8f9fb38a11
umang232 <umangsavla2323@gmail.com>
2023-09-27 18:53:05 -0400

    add, remove country list for player along with getPlayerName and getPlayerId added

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,15 +1,13 @@
 package model;
-
-
+import java.util.ArrayList;
 
 public class Player {
-
     private String d_PlayerName="";
     private GameModelNew d_GameModelNew;
     private int d_PlayerId;
 	private String d_PlayerColor = "";
-
-    
+	private ArrayList<Country> d_Countries = new ArrayList<Country>();
+	
     /**
 	 * default constructor of Player class	
 	 */
@@ -33,4 +31,33 @@ public class Player {
 		d_PlayerId = p_PlayerId;
 		d_PlayerColor = p_PlayerColor;
 	}
+	/**
+	 * addCountry method adds the given country to the player's country list.
+	 * @param p_Country country object
+	 */
+	public void addCountry(Country p_Country) {
+		d_Countries.add(p_Country);
+	}
+	/**
+	 * removeCountry removes the given country from the player's country list
+	 * @param p_Country Name of the country to be removed
+	 */
+	public void removeCountry(Country p_Country) {
+		d_Countries.remove(p_Country);
+	}
+	/**
+	 * get method for player name
+	 * @return returns player name
+	 */
+	public String getPlayerName() {
+		return this.d_PlayerName;
+	}
+	/**
+	 * get method for player id 
+	 * @return returns player id
+	 */
+	public int getPlayerId() {
+		return this.d_PlayerId;
+	}
+
 }
