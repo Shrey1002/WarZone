9d3d8415e057858c1d526084d326662fe2da69db
umang232 <umangsavla2323@gmail.com>
2023-09-29 19:42:32 -0400

    added constructor for PlayerController

--- src/main/java/controller/PlayerController.java
+++ src/main/java/controller/PlayerController.java
@@ -1,5 +1,18 @@
 package controller;
 
+import model.GameModelNew;
+import view.CommandPrompt;
+
 public class PlayerController {
+    /**
+	 * Constructor of Player controller
+	 * @param p_Players list of players 
+	 * @param p_CpView object of command prompt for communicating with player
+	 */
+	PlayerController(GameModelNew p_GameModelNew,CommandPrompt p_CpView) {
+		d_GameModelNew = p_GameModelNew;
+		d_Players = d_GameModelNew.getAllPlayers();
+		d_CpView=p_CpView;
+	}
     
 }
